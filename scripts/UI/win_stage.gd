8605814bd802d2cc9782e3e74df128780840c949
Andrei Esteves <qjogos7@gmail.com>
2026-01-08 00:28:59 +0000

    Fixing the final message not bring the user back to menu!

--- scripts/UI/win_stage.gd
+++ scripts/UI/win_stage.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var CongratsLabel: Label = $VBoxContainer/Label
 @onready var ScoreLabel: Label = $VBoxContainer/ScoreLabel
+const MENU = preload("uid://368oh7vd8o8f")
 
 func _ready() -> void:
 	ScoreLabel.text = "Score: " + str(Global.player_score + 200)
@@ -10,3 +11,7 @@ func _ready() -> void:
 
 	CongratsLabel.text = "Thank You For Playing!"
 	ScoreLabel.text = "More coming soon!"
+
+	await get_tree().create_timer(10).timeout
+	
+	get_tree().change_scene_to_packed(MENU)
